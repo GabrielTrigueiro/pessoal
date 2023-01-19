c450fc9704249b2002a5454ec1a87d1c5f7099f6
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-19 10:31:07 -0300

    CSS global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import { Layout } from '../components/Layout'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+    <>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
+        </Head>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    </>
   )
 }
