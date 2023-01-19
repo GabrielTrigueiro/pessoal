b04aa72df6651f080b02c6874d2558391d5dbefc
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-19 17:24:31 -0300

    CSS navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
-    return(
-        <div>
-            navbar
-        </div>
+    return (
+        <nav className={styles.navbar_container}>
+            <ul>
+                <li>
+                    <Link href={'/'}>Home</Link>
+                </li>
+                <li>
+                    <Link href={'/projetos'}>Projetos</Link>
+                </li>
+                <li>
+                    <Link href={'/sobre'}>Sobre mim</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
