7a4ee3fe72a0f5b4d58f0fe2c526b2a182b29ccb
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-23 08:28:43 -0300

    #CSS

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,16 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+    const api = 'https://api.github.com/users/GabrielTrigueiro/repos'
+
+    const res = await fetch(api)
+
+    const data = await res.json()
+
+    return {props: {data}}
+}
 
 export default function Projects() {
     return(
@@ -7,7 +19,7 @@ export default function Projects() {
                 <title>Projetos</title>
             </Head>
             <div>
-                pagina de projetos
+                {}
             </div>
         </>
     )
