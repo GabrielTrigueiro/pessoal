4ffdc8c82f24565a79a767a45ce577e5d0302caf
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-27 19:40:10 -0300

    começando a estilizar a tela de about

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,15 +2,40 @@ import Head from "next/head";
 import styles from '../styles/About.module.css'
 
 export default function About() {
+
+    const skills = [
+        "Typesciprt",
+        "Javascript",
+        "Html",
+        "CSS",
+        "Sass",
+        "React",
+        "Next",
+        "Flutter",
+        "Redux",
+        "React-Router",
+        "React-Hooks",
+        "Material-UI"
+    ]
+
     return(
-        <>
+        <>  
             <Head>
                 <title>Sobre</title>
             </Head>
             
             <div className={styles.container}>
-                <div className={styles.image}>oisas</div>
+                <div className={styles.image}>
+                    oi
+                </div>
+                <div className={styles.skills}>
+                    {skills.map((value) => (
+                        <div className={styles.item}>
+                            <p>{value}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     )
-}
+}   
